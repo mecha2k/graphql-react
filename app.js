6791ffd4e426ec18f45bd17e81e2fd123cca21c2
mecha2k <mecha2k@naver.com>
2020-03-26 18:29:58 +0900

    P65creator 1st commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const createError = require("http-errors");
 
+const graphQl = require("graphql");
+const graphqlHttp = require("express-graphql");
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -24,6 +27,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: null,
+    rootValue: {}
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
