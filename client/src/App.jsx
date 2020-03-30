91e05c2c12ae334226a86551fff321cd4055663f
mecha2k <mecha2k@naver.com>
2020-03-30 18:33:53 +0900

    P65creator 2nd commit (mongoDB Atlas)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return;
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,8 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>;
+  }
 }
 
 export default App;
