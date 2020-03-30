44ff3e5f79a05bc2819e52de564546aafde17eb4
mecha2k <mecha2k@naver.com>
2020-03-30 14:54:14 +0900

    P65creator 1st commit

---
+++ models/author.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const authorSchema = new schema({ name: String, age: Number });
+
+module.exports = mongoose.model("author", authorSchema);
---
+++ models/book.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const bookSchema = new schema({ name: String, genre: String, authorid: String });
+
+module.exports = mongoose.model("book", bookSchema);
